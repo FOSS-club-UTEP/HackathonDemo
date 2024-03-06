f22508f00b7210d517aa590c06a5177b02dff07f
toastloop <69254179+toastloop@users.noreply.github.com>
2024-03-06 15:40:00 -0700

    Added HelloWorld class

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Hello World Class
+ * 
+ * @author Matthew Knowlton
+ * @version 1.0
+ * @since 2024-03-06
+ */
+public class HelloWorld {
+    /**
+     * Prints "Hello, World"
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
